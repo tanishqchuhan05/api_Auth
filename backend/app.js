79cebea24439438277d70213e9ad2a1307e3807c
tanishqchauhan05 <tanishqchauhanwins@gmail.com>
2025-03-24 11:27:44 +0530

    fix cors error11111

--- backend/app.js
+++ backend/app.js
@@ -23,12 +23,15 @@ const app = express();
 // FIXED CORS CONFIGURATION
 const corsOptions = {
   origin: ["http://localhost:3000", 
-    "https://api-auth-la58.vercel.app/"], //Allow frontend URL
+    "https://api-auth-la58.vercel.app"], //Allow frontend URL
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Fixed methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow token headers
   credentials: true, // Required for authentication
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+
 
 app.get("/test", (req, res)=>{
   res.json({message: "backend is running"});
